11da0fc1b54ead182f8674cc753a7a575aab8d9f
samini3000 <saeedeh.amini3000@gmail.com>
2026-05-09 17:22:35 +0330

    add descrption

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -7,6 +7,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        //what is this project goal
+        /*  | Approach              | Thread Blocked? | Scalable? | Memory Usage | High Concurrency |
+            | --------------------- | --------------- | --------- | ------------ | ---------------- |
+            | Thread.Sleep          | YES             | Poor      | High         | Bad              |
+            | `.Wait()` / `.Result` | YES             | Poor      | High         | Bad              |
+            | `await`               | NO              | Excellent | Low          | Excellent        |
+        */
         BenchmarkRunner.Run<ReportBenchmark>();
     }
 }
