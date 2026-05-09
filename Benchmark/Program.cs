d0a927935361c36e6ed82b7c4976daec8ac99275
samini3000 <saeedeh.amini3000@gmail.com>
2026-05-09 17:23:58 +0330

    benchmark result

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -14,6 +14,14 @@ public class Program
             | `.Wait()` / `.Result` | YES             | Poor      | High         | Bad              |
             | `await`               | NO              | Excellent | Low          | Excellent        |
         */
+
+        //benchmark output
+        /*| Method            | Mean       | Error    | StdDev   | Allocated |
+        |------------------ |-----------:|---------:|---------:|----------:|
+        | AsyncGetReport    |   618.7 ms |  1.77 ms |  1.57 ms |    1944 B |
+        | BadAsyncGetReport | 6,260.4 ms | 37.96 ms | 35.51 ms |   23200 B |
+        | SyncGetReport     | 6,236.7 ms |  8.58 ms |  8.02 ms |         - |
+        */
         BenchmarkRunner.Run<ReportBenchmark>();
     }
 }
